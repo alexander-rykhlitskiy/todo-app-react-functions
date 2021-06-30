3f421538fd30432f9ef97281a0aa62ae89047fbf
Alexander Ryhlitsky <alexander.rykhlitskiy@gmail.com>
2021-06-30 19:48:12 +0300

    Add the simplest form and list

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+
+  const [items, setItems] = useState([]);
+  const [currentItemValue, setCurrentItemValue] = useState('');
+
+  const submitForm = function(event) {
+    event.preventDefault();
+    setItems([...items, { title: currentItemValue }]);
+    setCurrentItemValue('');
+  }
+
+  const setCurrentItemValueInput = function(event) {
+    setCurrentItemValue(event.target.value);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <form onSubmit={submitForm}>
+          <input placeholder="Enter task" type="text" value={currentItemValue} onChange={setCurrentItemValueInput}></input>
+          <input type="submit"></input>
+        </form>
+      </div>
+      <ul>
+        {items.map((item, index) => <li key={index}>{item.title}</li>)}
+      </ul>
     </div>
   );
 }
